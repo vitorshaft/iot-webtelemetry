23e98d69fce8d93e278c59dc09334c593d589e23
Vitor Domingues <vtrshaft@gmail.com>
2026-03-23 11:13:49 -0300

    DB URL failsafe

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from sqlalchemy.orm import sessionmaker, Session
 
 # Configurações de Banco de Dados
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
